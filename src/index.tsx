8cb4204b343991676799ba6b85f306ecc8955cca
maruson08 <maruson08@naver.com>
2026-02-04 11:25:55 +0900

    :bug:FIx Kakao Ad position

--- src/index.tsx
+++ src/index.tsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <div />
         <KakaoAD />
     </React.StrictMode>,
 )
