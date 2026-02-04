e5ba237841de98f87dfae16702a2bb1142ff072b
maruson08 <maruson08@naver.com>
2026-02-04 11:35:48 +0900

    :bug: Negative column value

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <RouterProvider router={router} />
-        <div />
         <KakaoAD />
     </React.StrictMode>,
 )
